8be350ccf9872c69ca612cfacf9c909999e2262a
Berkin <bkabadayi@ziraatteknoloji.com>
2023-07-05 21:49:36 +0300

    Color extension added.

---
+++ RaveCrypto/Extensions/ColorExtension.swift
@@ -0,0 +1,20 @@
+//
+//  ColorExtension.swift
+//  RaveCrypto
+//
+//  Created by Berkin Kabadayı on 5.07.2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
