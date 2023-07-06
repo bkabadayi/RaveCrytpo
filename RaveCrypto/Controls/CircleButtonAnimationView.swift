207f60cbc4f47473146eef638469030257bc5b56
Berkin <bkabadayi@ziraatteknoloji.com>
2023-07-06 10:33:30 +0300

    CircleButtonAnimationView has been added.

---
+++ RaveCrypto/Controls/CircleButtonAnimationView.swift
@@ -0,0 +1,29 @@
+//
+//  CircleButtonAnimationView.swift
+//  RaveCrypto
+//
+//  Created by Berkin Kabadayı on 6.07.2023.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+    
+    @Binding var animate: Bool
+    
+    var body: some View {
+        Circle()
+            .stroke(lineWidth: 5.0)
+            .scale(animate ? 1.0 : 0.0)
+            .animation(animate ? .easeOut : .none,
+                       value: 0)
+    }
+}
+
+struct CircleButtonAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleButtonAnimationView(animate: .constant(false))
+            .foregroundColor(.red)
+            .frame(width: 100, height: 100)
+    }
+}
--- RaveCrypto/Views/HomeView.swift
+++ RaveCrypto/Views/HomeView.swift
@@ -17,27 +17,36 @@ struct HomeView: View {
                 .ignoresSafeArea()
             
             VStack {
-                HStack {
-                    CircleButtonView(iconName: showPortfolio ? "plus" : "info")
-                        .animation(.none, value: 0)
-                    Spacer()
-                    Text(showPortfolio ? "Portfolio" : "Live Prices")
-                        .font(.headline)
-                        .fontWeight(.heavy)
-                        .foregroundColor(.theme.accent)
-                        .animation(.none)
-                    Spacer()
-                    CircleButtonView(iconName: "chevron.right")
-                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
-                        .onTapGesture {
-                            withAnimation(.spring()) {
-                                showPortfolio.toggle()
-                            }
-                        }
-                }
+                homeHeader
                 Spacer(minLength: 0)
             }
         }
+    }
+}
+
+extension HomeView {
+    
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none,
+                           value: 0)
+                .background(CircleButtonAnimationView(animate: $showPortfolio))
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }.padding(.horizontal)
     }
 }
 
