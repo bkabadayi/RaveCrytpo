af7c1aa333feae4e4d4b858cc3694a31bc3caadd
Berkin <bkabadayi@ziraatteknoloji.com>
2023-07-06 10:19:39 +0300

    CircleButtonView and HomeView has been added.

---
+++ RaveCrypto/Views/HomeView.swift
@@ -0,0 +1,51 @@
+//
+//  HomeView.swift
+//  RaveCrypto
+//
+//  Created by Berkin Kabadayı on 5.07.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio = false
+    
+    var body: some View {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                        .animation(.none, value: 0)
+                    Spacer()
+                    Text(showPortfolio ? "Portfolio" : "Live Prices")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(.theme.accent)
+                        .animation(.none)
+                    Spacer()
+                    CircleButtonView(iconName: "chevron.right")
+                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                showPortfolio.toggle()
+                            }
+                        }
+                }
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .toolbar(.hidden)
+        }
+    }
+}
